e89e9d7771b589d1da41e3ad7c094689e842b845
ImChri2 <chris@chris-industries.de>
2023-01-13 11:02:31 +0100

    fixed Servo issue

--- src/remoteapp.hpp
+++ src/remoteapp.hpp
@@ -4,6 +4,6 @@
 int readSensor(int sensor_right, int sensor_left);
 int joystick (int xy);
 int set_control_mode(bool autopilot);
-void control_servo(bool servo_val);
+int control_servo(bool servo_val);
 
 #endif
